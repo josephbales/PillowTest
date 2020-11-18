5bd99e760e7fdf5c8d5cfe592e33015075f167d4
Joseph Bales <joseph.bales@gmail.com>
2020-11-17 21:01:38 -0600

    wrote a bit of code to generate the bitmap I want using PIL instead of trying to write an svg

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
+
+def process_message(drawing, message_text, font):
+    message_text = message_text.replace('\r\n', ' ')
+    message_text = message_text.replace('\n', ' ')
+    message_text = message_text.replace('  ', ' ')
+    number_of_lines = 1
+    w, h = drawing.textsize(message_text, font=font)
+    if w < 600:
+        return message_text, number_of_lines, w, h
+
+    number_of_lines = 2
+    spaces = find_occurrences(message_text, ' ')
+    for i in range(len(spaces) - 1, 0, -1):
+        message_text = message_text.replace('\n', ' ')
+        msg_list = list(message_text)
+        msg_list[spaces[i]] = '\n'
+        message_text = ''.join(msg_list)
+        w, h = drawing.textsize(message_text, font=font)
+        if w < 600:
+            return message_text, number_of_lines, w, h
+
+    return None
+
+
+def find_occurrences(s, ch):
+    return [i for i, letter in enumerate(s) if letter == ch]
+
+
 # create an image
 img = Image.new(mode='L', size=(630, 374), color=255)
 
@@ -20,8 +48,8 @@ temperature = '108°'
 w, h = d.textsize(temperature, font=temp_fnt)
 x_coord = (240 - w) / 2
 d.multiline_text((x_coord, 190), temperature, font=temp_fnt)
-d.multiline_text((260, 13), 'Current Conditions:', font=cc_fnt)
-d.multiline_text((260, 37), 'Severe Thunderstorms', font=cond_fnt)
+d.multiline_text((260, 12), 'Current Conditions:', font=cc_fnt)
+d.multiline_text((260, 32), 'Severe Thunderstorms', font=cond_fnt)
 d.multiline_text((260, 80), 'Feels Like: 108°', font=cc_fnt)
 d.multiline_text((260, 102), 'Relative Humidity: 80%', font=cc_fnt)
 d.multiline_text((260, 124), 'Barometric Pressure: 30.92 inches', font=cc_fnt)
@@ -29,10 +57,21 @@ d.multiline_text((260, 146), 'Wind Speed: 28 mph', font=cc_fnt)
 d.multiline_text((260, 168), 'Wind Direction: NNW', font=cc_fnt)
 d.multiline_text((260, 204), 'Sunrise: 07:26:59 CST', font=cc_fnt)
 d.multiline_text((260, 226), 'Sunset: 19:26:59 CST', font=cc_fnt)
-d.multiline_text((260, 265), 'Updated at: 2020-11-17 19:26:59 CST', font=cc_fnt)
+d.multiline_text((260, 262), 'Updated at: 2020-11-17 19:26:59 CST', font=cc_fnt)
 
 # Probably need a 100 char limit here to make sure it all fits in the box
-d.multiline_text((15, 300), "You can just push a little tree out of your brush like that. I\nthought today we would do a happy little picture. Everybody's\ndifferent. Trees are different. Let them all be individuals.", font=cc_fnt)
+message = "You can just push a little tree out of your brush like that. We can do a happy little picture today."
+#message = "You can just push a little tree out of your brush like that."
+msg, lines, width, height = process_message(d, message, cc_fnt)
+print(msg)
+print(lines)
+print(width)
+print(height)
+y_coord = 312
+if lines == 1:
+    y_coord = 322
+x_coord = (630 - width) / 2
+d.multiline_text((x_coord, y_coord), msg, font=cc_fnt)
 
 # draw lines
 d.line([(240, 0), (240, 300)], fill=0, width=5)
